d3d5c02b6bd2e1337b07f672286da09ddf38033a
Sabrina Bezerra <sabrinabezsilva@gmail.com>
2022-03-02 18:19:16 -0300

    fixing part of error when adding a meme in padMemes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const botoes = document.querySelectorAll("div.btn-formatacao");
-const sons = document.querySelectorAll("audio");
-const img = document.querySelectorAll("img.foto");
+const sons = document.getElementsByTagName("audio");
+let img = document.querySelectorAll("img.foto");
 const corpo = document.querySelector("body");
 
 lista_sons = ['som-inez', 'som-flay', 'som-juliette', 'som-karol', 'som-regina', 'som-nadaAverIrmao', 'som-melody', 'som-juninhoCaldeirao', 'som-ygona']
@@ -16,12 +16,13 @@ class Pad{
         this.lista.push(nome)
     }
 }
+
 const padMemes = new Pad(botoes, sons, img, lista_sons)
 
 let verificador = 0;
 let ultimoValorI = null
 function tocar(som){
-    console.log("entrou no tocar")
+    img = document.querySelectorAll("img.foto");
     window.scrollTo({//quando tocar em um botão a tela scrolla pra cima para o usuário ver a imagem
         top:0,
         behavior: 'smooth'
@@ -32,15 +33,14 @@ function tocar(som){
                 if(ultimoValorI != null){//se ultimoValorI for diferente de null quer dizer que ja foi tocado um som e tem uma imagem na tela
                     padMemes.sons[ultimoValorI].pause()
                     padMemes.sons[ultimoValorI].currentTime = 0;
-                    padMemes.imagens[ultimoValorI].style.display = 'none'//tira essa ultima imagem da tela
-                    padMemes.imagens[i].style.display = 'flex'//coloca a nova imagem
+                    img[ultimoValorI].style.display = 'none'//tira essa ultima imagem da tela
+                    img[i].style.display = 'flex'//coloca a nova imagem
                     padMemes.sons[i].play()// toca o som correspondente a imagem
                     ultimoValorI = i//guarda qual foi o ultimo som e imagem
-                    
                     break
                 }
                 else{//é p primeiro som/imagem a ser reproduzido
-                    padMemes.imagens[i].style.display = 'flex'//mostrar a imagem
+                    img[i].style.display = 'flex'//mostrar a imagem
                     padMemes.sons[i].play()//tocar som
                     verificador++//mostra ao programa que o próximo som/imagem a ser reproduzido não é o 1°
                     ultimoValorI = i//guarda o som/imagem que foi reproduzido
@@ -50,8 +50,8 @@ function tocar(som){
             else{//não é o 1° som/imagem a ser reproduzido
                 padMemes.sons[ultimoValorI].pause()
                 padMemes.sons[ultimoValorI].currentTime = 0;
-                padMemes.imagens[ultimoValorI].style.display = 'none';
-                padMemes.imagens[i].style.display = 'flex';
+                img[ultimoValorI].style.display = 'none';
+                img[i].style.display = 'flex';
                 padMemes.sons[i].play()
                 ultimoValorI = i
                 break
@@ -71,24 +71,16 @@ const adicionar = document.querySelector("input#adicionar")
 let input_audio = document.querySelector("input#audio")
 let input_imagem = document.querySelector("input#imagem")
 
-var uploadAudio = ""
-var uploadImg = ""
-input_audio.addEventListener("change", function(){
-    const reader = new FileReader()//criou um leitor de arquivo
-    reader.addEventListener("load", ()=>{//quando esse leitor carregar o arquivo
-    uploadAudio = reader.result//pega o resultado desse arquivo
-    console.log(uploadAudio)
-    })
-})
-input_imagem.addEventListener("change", function(){
-    const reader = new FileReader()
-    reader.addEventListener("load", ()=>{
-    uploadImg = reader.result
-    })
-})
+let imagem_url;
+let audio_url;
 
-adicionar.addEventListener("click", ()=>{
-    window.alert("Função ainda em construção!")
+input_imagem.addEventListener("change", (event)=>{
+    imagem_url = URL.createObjectURL(event.target.files[0])
+})
+input_audio.addEventListener("change", (event)=>{
+    audio_url = URL.createObjectURL(event.target.files[0])
+})
+adicionar.addEventListener("click", (event)=>{
     let input_nome = document.querySelector("input#nome").value//pegando valores dos inputs
     let input_cor = document.querySelector("input#cor").value
     
@@ -111,20 +103,16 @@ adicionar.addEventListener("click", ()=>{
     //criando tag IMG
     imagem = document.createElement("img")
     imagem.classList.toggle("foto")
-    imagem.src = uploadImg
+    imagem.src = imagem_url
     const container_img = document.querySelector("div.imagens-container")
     container_img.appendChild(imagem)//jogando tag IMG no container de imgs
 
     //criando tag AUDIO
     audio = document.createElement("AUDIO")
-    audio.src = uploadAudio
+    audio.src = audio_url
     audio.classList.toggle(input_nome)//colocando uma classe
     const container_sons = document.querySelector("div.sons-container")
     container_sons.appendChild(audio)//jogando tag AUDIO no container de audios
-    
-    div.addEventListener("click", ()=>{
-        tocar(div.className)
-    })
+
+    addEventListener("click", tocar(input_nome))
 })
-
-
